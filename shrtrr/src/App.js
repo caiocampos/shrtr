2b01c2995a7b28681af177ec84a80f048915cfca
Caio Campos <caio.campos1204@gmail.com>
2019-12-20 23:23:03 -0300

    teste

--- shrtrr/src/App.js
+++ shrtrr/src/App.js
@@ -3,7 +3,7 @@ import Routes from './components/Routes';
 import { BrowserRouter } from 'react-router-dom';
 
 const App = props => (
-	<BrowserRouter {/*basename="/shrtr"*/}>
+	<BrowserRouter>
 		<Routes />
 	</BrowserRouter>
 );
