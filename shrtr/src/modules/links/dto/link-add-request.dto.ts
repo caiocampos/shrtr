37443156616624c0253afda6fb7579c4d42bf44a
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2024-12-18 21:24:28 -0300

    chore: changed class decorations

--- shrtr/src/modules/links/dto/link-add-request.dto.ts
+++ shrtr/src/modules/links/dto/link-add-request.dto.ts
@@ -1,9 +1,10 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 import { ValidationMessages } from '../../../common/messages/validation-messages.constants';
 
 export default class LinkAddRequestDTO {
   @IsNotEmpty({ message: ValidationMessages.IS_NOT_EMPTY })
   link: string;
 
+  @IsOptional()
   shrt?: string;
 }
