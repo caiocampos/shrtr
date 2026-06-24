696f4872045d0c856bc196d133206e1387f36514
caiocampos <20524962+caiocampos@users.noreply.github.com>
2026-06-24 14:04:49 +0000

    Deploying to gh-pages from @ caiocampos/shrtr@bbe2cb9d5088dce3250f328c7ada60e0848bb9f0 🚀

--- assets/Wrapper-mw72iNB5.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./react-BSd5bZJk.js";var t={Wrapper:`_Wrapper_1eg4z_1`},n=e(),r=({className:e,children:r})=>(0,n.jsx)(`div`,{className:t.Wrapper,children:(0,n.jsx)(`span`,{className:e,children:r})});export{r as t};
